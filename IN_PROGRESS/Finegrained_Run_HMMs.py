be1fcf05ae203b605d6951e3a480cc2e5faf6db2
spykard <kardakis@ceid.upatras.gr>
2019-03-12 06:36:38 +0200

    Temp

--- IN_PROGRESS/Finegrained_Run_HMMs.py
+++ IN_PROGRESS/Finegrained_Run_HMMs.py
@@ -67,7 +67,7 @@ if False:
 #       2nd Framework Training Settings (High-Order done through the 'hohmm_high_order' parameter)
 #       Any Framework Prediction Settings (Architecture B)
 
-if True:
+if False:
     # Model
     general_mixture_model_labels = HMM_Framework.general_mixture_model_label_generator(df.loc[:,"Sequences"], df.loc[:,"Labels"])
     hmm = HMM_Framework.HMM_Framework()
@@ -81,20 +81,17 @@ if True:
             architecture_b_algorithm="forward"                                                                                             \
             )     
     
-# ALREADY NOTICED THAT ON GAUSSIAN MIXTURE, USING DUMMY FLAG ON n_grams=3 IMPROVES PERFORMANCE (no shuffling)
-# ALSO NOT USING SHUFFLING LEADS TO MUCH BETTER PERFORMANCE ON n_grams=3
-
-elif False:
+elif True:
     #  Model
-    #  Just for State-emission HMM, remember to remove the "mix" label during preprocessing.
+    #  Just for State-emission HMM, might need to remove the "mix" label during preprocessing.
     hmm = HMM_Framework.HMM_Framework()
-    hmm.build(architecture="B", model="State-emission HMM", framework="pome", k_fold=5,                                                   \
+    hmm.build(architecture="B", model="State-emission HMM", framework="hohmm", k_fold=5,                                                   \
             state_labels_pandas=df.loc[:,"Sequences"], observations_pandas=df.loc[:,"Sequences"], golden_truth_pandas=df.loc[:,"Labels"], \
             text_instead_of_sequences=[], text_enable=False,                                                                              \
-            n_grams=1, n_target="", n_prev_flag=False, n_dummy_flag=False,                                                                \
-            pome_algorithm="baum-welch", pome_verbose=False, pome_njobs=1, pome_smoothing_trans=0.0, pome_smoothing_obs=0.0,              \
+            n_grams=1, n_target="both", n_prev_flag=False, n_dummy_flag=False,                                                           \
+            pome_algorithm="baum-welch", pome_verbose=False, pome_njobs=1, pome_smoothing_trans=0.0, pome_smoothing_obs=0.2,              \
             pome_algorithm_t="map",                                                                                                       \
-            hohmm_high_order=1, hohmm_smoothing=0.0, hohmm_synthesize=False,                                                              \
+            hohmm_high_order=2, hohmm_smoothing=0.0, hohmm_synthesize=False,                                                              \
             architecture_b_algorithm="forward"                                                                                             \
             )   
 

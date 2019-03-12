5900331082d402f54759d301f0bda8d898affb33
spykard <kardakis@ceid.upatras.gr>
2019-03-12 04:28:30 +0200

    Temp

--- IN_PROGRESS/Finegrained_Run_HMMs.py
+++ IN_PROGRESS/Finegrained_Run_HMMs.py
@@ -67,46 +67,38 @@ if False:
 #       2nd Framework Training Settings (High-Order done through the 'hohmm_high_order' parameter)
 #       Any Framework Prediction Settings (Architecture B)
 
-if False:
+if True:
     # Model
     general_mixture_model_labels = HMM_Framework.general_mixture_model_label_generator(df.loc[:,"Sequences"], df.loc[:,"Labels"])
     hmm = HMM_Framework.HMM_Framework()
-    hmm.build(architecture="A", model="General Mixture Model", framework="pome", k_fold=5, \
+    hmm.build(architecture="B", model="General Mixture Model", framework="hohmm", k_fold=5,                                                 \
             state_labels_pandas=general_mixture_model_labels, observations_pandas=df.loc[:,"Sequences"], golden_truth_pandas=df.loc[:,"Labels"], \
             text_instead_of_sequences=[], text_enable=False,                                                                              \
-            n_grams=1, n_target="", n_prev_flag=False, n_dummy_flag=False,                                                                \
+            n_grams=2, n_target="obs", n_prev_flag=False, n_dummy_flag=False,                                                                \
             pome_algorithm="baum-welch", pome_verbose=False, pome_njobs=1, pome_smoothing_trans=0.0, pome_smoothing_obs=0.0,              \
             pome_algorithm_t="map",                                                                                                       \
-            hohmm_high_order=1, hohmm_smoothing=0.0, hohmm_synthesize=False,                                                                                   \
+            hohmm_high_order=1, hohmm_smoothing=0.0, hohmm_synthesize=False,                                                              \
             architecture_b_algorithm="forward"                                                                                             \
             )     
     
-    hmm.print_average_results()
-
-
 # ALREADY NOTICED THAT ON GAUSSIAN MIXTURE, USING DUMMY FLAG ON n_grams=3 IMPROVES PERFORMANCE (no shuffling)
 # ALSO NOT USING SHUFFLING LEADS TO MUCH BETTER PERFORMANCE ON n_grams=3
 
-elif True:
+elif False:
     #  Model
     #  Just for State-emission HMM, remember to remove the "mix" label during preprocessing.
     hmm = HMM_Framework.HMM_Framework()
-    hmm.build(architecture="B", model="State-emission HMM", framework="hohmm", k_fold=5,                                                   \
+    hmm.build(architecture="B", model="State-emission HMM", framework="pome", k_fold=5,                                                   \
             state_labels_pandas=df.loc[:,"Sequences"], observations_pandas=df.loc[:,"Sequences"], golden_truth_pandas=df.loc[:,"Labels"], \
             text_instead_of_sequences=[], text_enable=False,                                                                              \
             n_grams=1, n_target="", n_prev_flag=False, n_dummy_flag=False,                                                                \
             pome_algorithm="baum-welch", pome_verbose=False, pome_njobs=1, pome_smoothing_trans=0.0, pome_smoothing_obs=0.0,              \
             pome_algorithm_t="map",                                                                                                       \
-            hohmm_high_order=1, hohmm_smoothing=0.0, hohmm_synthesize=False,                                                                                   \
+            hohmm_high_order=1, hohmm_smoothing=0.0, hohmm_synthesize=False,                                                              \
             architecture_b_algorithm="forward"                                                                                             \
             )   
 
     # NEW FEATURE FOR 'FORMULA' parameter 'formula_magic_smoothing' that doesn't crash when out-of-vocabulary
-    hmm.print_best_results(decimals=3)    
-    hmm.print_average_results(decimals=3)
-
-    #print(hmm.cross_val_prediction_matrix)
-
-
-
-# self.cross_val_prediction_matrix
+   
+hmm.print_average_results(decimals=3)
+hmm.print_best_results(detailed=False, decimals=3) 

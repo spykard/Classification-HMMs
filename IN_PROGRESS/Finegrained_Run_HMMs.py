c6fe53d38012aeca8e2f500eeddcd78ccbc7b6e8
spykard <kardakis@ceid.upatras.gr>
2019-03-13 11:24:47 +0200

    Temp

--- IN_PROGRESS/Finegrained_Run_HMMs.py
+++ IN_PROGRESS/Finegrained_Run_HMMs.py
@@ -72,10 +72,10 @@ if False:
     # Model
     general_mixture_model_labels = HMM_Framework.general_mixture_model_label_generator(df.loc[:,"Sequences"], df.loc[:,"Labels"])
     hmm = HMM_Framework.HMM_Framework()
-    hmm.build(architecture="B", model="General Mixture Model", framework="pome", k_fold=5,                                                 \
+    hmm.build(architecture="A", model="General Mixture Model", framework="hohmm", k_fold=5,                                                 \
             state_labels_pandas=general_mixture_model_labels, observations_pandas=df.loc[:,"Sequences"], golden_truth_pandas=df.loc[:,"Labels"], \
             text_instead_of_sequences=[], text_enable=False,                                                                              \
-            n_grams=1, n_target="obs", n_prev_flag=False, n_dummy_flag=False,                                                                \
+            n_grams=1, n_target="both", n_prev_flag=False, n_dummy_flag=False,                                                                \
             pome_algorithm="baum-welch", pome_verbose=False, pome_njobs=1, pome_smoothing_trans=0.0, pome_smoothing_obs=0.0,              \
             pome_algorithm_t="map",                                                                                                       \
             hohmm_high_order=1, hohmm_smoothing=0.0, hohmm_synthesize=False,                                                              \
@@ -86,17 +86,15 @@ elif True:
     #  Model
     #  Just for State-emission HMM, might need to remove the "mix" label during preprocessing.
     hmm = HMM_Framework.HMM_Framework()
-    hmm.build(architecture="B", model="State-emission HMM", framework="pome", k_fold=5,                                                   \
+    hmm.build(architecture="B", model="State-emission HMM", framework="hohmm", k_fold=5,                                                   \
             state_labels_pandas=df.loc[:,"Sequences"], observations_pandas=df.loc[:,"Sequences"], golden_truth_pandas=df.loc[:,"Labels"], \
             text_instead_of_sequences=[], text_enable=False,                                                                              \
             n_grams=1, n_target="both", n_prev_flag=False, n_dummy_flag=False,                                                           \
-            pome_algorithm="baum-welch", pome_verbose=False, pome_njobs=1, pome_smoothing_trans=0.0, pome_smoothing_obs=0.0,              \
+            pome_algorithm="baum-welch", pome_verbose=False, pome_njobs=1, pome_smoothing_trans=0.2, pome_smoothing_obs=0.2,              \
             pome_algorithm_t="map",                                                                                                       \
-            hohmm_high_order=1, hohmm_smoothing=0.0, hohmm_synthesize=False,                                                              \
-            architecture_b_algorithm="formula", formula_magic_smoothing=0.0                                                                                             \
+            hohmm_high_order=2, hohmm_smoothing=0.0, hohmm_synthesize=False,                                                              \
+            architecture_b_algorithm="forward", formula_magic_smoothing=0.0                                                                                             \
             )   
 
-
-print(np.mean(hmm.cross_val_metrics["Time_Complexity"]))
 hmm.print_average_results(decimals=3)
 hmm.print_best_results(detailed=False, decimals=3) 

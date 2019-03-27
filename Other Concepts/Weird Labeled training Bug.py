1b3644c11645036bf56e887e1ec99c90ac41ae8f
spykard <kardakis@ceid.upatras.gr>
2019-03-27 18:29:42 +0200

    Unfixable

--- Other Concepts/Weird Labeled training Bug.py
+++ Other Concepts/Weird Labeled training Bug.py
@@ -4,16 +4,66 @@
 # e.g. hmm_supervised_pos = HiddenMarkovModel.from_samples(DiscreteDistribution, n_components=3, X=pos_data_corresponding_to_labels, algorithm='labeled', verbose=True, labels=pos_clustered_labeled_data, n_jobs=1)
 
 from pomegranate import *
+import numpy as np
 
 # Happens in this example
 pos_clustered_labeled_data = [["dummy1", "pos", "neg", "neg", "dummy1"], ["dummy1", "pos"]]
 pos_data_corresponding_to_labels = [["dummy1", "good", "bad", "bad", "whateveromegalul"], ["dummy1", "good"]]
 
-# builder = Builder()
-# builder.add_batch_training_examples(pos_data_corresponding_to_labels, pos_clustered_labeled_data)
-# hmm = builder.build()
-# hmm.display_parameters()
-# quit()
+# New Idea
+#print(hmm_supervised_pos.states.index(hmm_supervised_pos.states[2]))
+#print(hmm_supervised_pos.dense_transition_matrix()[:-2,:-2])
+# from_matrix function
+
+distribution = DiscreteDistribution
+
+d1 = DiscreteDistribution.from_samples(pos_data_corresponding_to_labels[0])
+d2 = DiscreteDistribution.from_samples(pos_data_corresponding_to_labels[0])
+d3 = DiscreteDistribution.from_samples(pos_data_corresponding_to_labels[0])
+
+print(type(d1), d1)
+
+s1 = State(d1, name="dummy1")
+s2 = State(d2, name="pos")
+s3 = State(d3, name="neg")
+
+model = HiddenMarkovModel('example')
+model.add_states([s1, s2, s3])
+model.add_transition(model.start, s1, 0.33)
+model.add_transition(model.start, s2, 0.33)
+model.add_transition(model.start, s3, 0.33)
+model.add_transition(s1, s1, 0.33)
+model.add_transition(s1, s2, 0.33)
+model.add_transition(s1, s3, 0.33)
+model.add_transition(s2, s1, 0.33)
+model.add_transition(s2, s2, 0.33)
+model.add_transition(s2, s3, 0.33)
+model.add_transition(s3, s1, 0.33)
+model.add_transition(s3, s2, 0.33)
+model.add_transition(s3, s3, 0.33)
+model.bake(merge = "All")
+
+
+state_names = [state.name for state in model.states]
+state_indices = [[state_names.index(s) for s in seq] for seq in pos_clustered_labeled_data]
+state_values = [[model.states[i] for i in seq] for seq in state_indices]
+
+model.fit(sequences=pos_data_corresponding_to_labels, algorithm='labeled', verbose=True, labels=state_values, n_jobs=1)
+#quit()
+print("NEXT HMM")
+for i in list(model.states):
+    print(i.name)
+print(model)
+
+import matplotlib.pyplot as plt
+fig, ax1 = plt.subplots()
+fig.canvas.set_window_title("Hidden Markov Model Graph")
+model.plot()
+plt.show()
+#quit()
+
+pos_clustered_labeled_data = [["dummy1", "pos", "neg", "neg", "dummy1"], ["dummy1", "pos"]]
+pos_data_corresponding_to_labels = [["dummy1", "good", "bad", "bad", "whateveromegalul"], ["dummy1", "good"]]
 
 # Training
 # Build Pos Class HMM - !!! state_names should be in alphabetical order
@@ -23,6 +73,7 @@ print("NEXT HMM")
 for i in list(hmm_supervised_pos.states):
     print(i.name)
 print(hmm_supervised_pos)
+print("edw")
 quit()
 
 # Also happens in this example without using state_names

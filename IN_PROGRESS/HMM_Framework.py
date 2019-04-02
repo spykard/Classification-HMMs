5f7d1f63e5f0bdd006bbcf93c53021c6b75fd79b
spykard <kardakis@ceid.upatras.gr>
2019-04-02 07:00:00 +0300

    Semi-final

--- IN_PROGRESS/HMM_Framework.py
+++ IN_PROGRESS/HMM_Framework.py
@@ -320,6 +320,8 @@ class HMM_Framework:
         print("# Transition probability matrix. One for each model that was trained.")
         print(self.A)
         print("# Observation probability matrix. One for each model that was trained.")
+        #xxx = list(self.B[0][0, :])
+        #second_smallest_value = sorted(list(set(xxx)))[1]  # Good way to find a value for formula_magic_smoothing, use with hohmm
         print(self.B)
         print("# Initial probabilities. One for each model that was trained.")
         print(self.pi)
